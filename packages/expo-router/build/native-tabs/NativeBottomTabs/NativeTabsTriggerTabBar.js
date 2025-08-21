995f3d689873d5265c6a277ff93ae0ea4c27c4e3
Jakub Tkacz <jtkacz@expo.io>
2025-08-21 20:39:09 +0200

    remove scroll edge appearance

---
+++ packages/expo-router/build/native-tabs/NativeBottomTabs/NativeTabsTriggerTabBar.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NativeTabsTriggerTabBar = NativeTabsTriggerTabBar;
+/**
+ * **Note:** You can use the alias `NativeTabs.Trigger.TabBar` for this component.
+ *
+ * The component used to customize the style of the tab bar, when given trigger is selected.
+ *
+ * Prefer this to global changes of tab bar styles, directly in the page
+ *
+ * @example
+ * ```tsx
+ * <NativeTabs
+ *  backgroundColor="black"
+ * >
+ *  <NativeTabs.Trigger name="page">
+ *    <NativeTabs.Trigger.TabBar
+ *      backgroundColor="white"
+ *    />
+ *    <Label>Page</Label>
+ *  </NativeTabs.Trigger>
+ * </NativeTabs>
+ *
+ */
+function NativeTabsTriggerTabBar(props) {
+    return null;
+}
+//# sourceMappingURL=NativeTabsTriggerTabBar.js.map
